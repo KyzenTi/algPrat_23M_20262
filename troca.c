ae8b061820a755f020e0173bc08e123dc03d039a
KyzenTi <62443451+KyzenTi@users.noreply.github.com>
2026-09-22 13:34:59 +0000

    Troca de vari´áveis

--- troca.c
+++ troca.c
@@ -2,15 +2,18 @@
 
 int main(){
 
-    int a,b,aux;
+    int a,b,c,aux;
 
-    a=10;
-    b=5;
+    a = 10;
+    b = 5;
+    c = 1;
     printf("a = %i, b= %i\n", a,b);
 
+   if (a > b){    
     aux = a;
     a = b;
     b = aux;
+   }
     
     printf("a = %i, b = %i\n", a,b);
 
