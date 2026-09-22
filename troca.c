fb42b56d62fe56985cdb37d3ae661fdbb33a75f9
KyzenTi <62443451+KyzenTi@users.noreply.github.com>
2026-09-22 14:06:27 +0000

    Troca de vari´áveis

--- troca.c
+++ troca.c
@@ -2,11 +2,12 @@
 
 int main(){
 
-    int a,b,c,aux;
+    int a,b,c,aux,i;
+    i = 0;
 
-    a = 10;
+    a = 5;
     b = 1;
-    c = 5;
+    c = 10;
     printf("Sequencia inicial: \na = %i, b= %i, c = %i\n\n", a,b,c);
 
     if (a > b) {  
@@ -14,19 +15,25 @@ int main(){
         a = b;
         b = aux;
         printf("1° troca: a = %i, b= %i, c = %i\n", a,b,c);
+        i++;
     } // a =5, b =10, c=1
+
     if ( b > c){
         aux = b;
         b = c;
         c = aux;
-        printf("2° troca: a = %i, b= %i, c = %i\n", a,b,c);   
+        printf("2° troca: a = %i, b= %i, c = %i\n", a,b,c);  
+        i++;
     } // a =5, b=1, c=10
+
     if ( a > b ){
         aux = a;
         a = b;
         b = aux;
         printf("3° troca: a = %i, b= %i, c = %i\n", a,b,c);
+        i++;
     }   
+    printf("\nQuantidade de 'ifs': %i\n", i );
 
     return 0;
 }
